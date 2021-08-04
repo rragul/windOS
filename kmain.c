04e179d4cdaa02f48a44ed38967ccb32ddf8a634
rragul <62707278+rragul@users.noreply.github.com>
2021-08-04 12:07:15 +0530

    Revert "implement_input_output (#3)" (#5)
    This reverts commit ffe373914568f0f71517045b4731d247cb077948.

--- kmain.c
+++ kmain.c
@@ -1,14 +1,5 @@
-#include "drivers/frame_buffer.h"
-#include "drivers/serial_port.h"
-
 /* The C function */
     int sum_of_three(int arg1, int arg2, int arg3)
     {
-    	char buffer[] = "Feel the Wind";
-    	
-  	fb_write(buffer, 19);
-  	serial_write(0x3F8, buffer, 19);
-    	
         return arg1 + arg2 + arg3;
     }
-    
