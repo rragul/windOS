e00e5baf6d8564d6fa26f0ecba971ea8bb4385b1
rragul <62707278+rragul@users.noreply.github.com>
2021-08-13 20:03:54 +0530

    integrate_segmentation

--- kmain.c
+++ kmain.c
@@ -1,13 +1,17 @@
 #include "drivers/frame_buffer.h"
 #include "drivers/serial_port.h"
+#include "segments/memory_segments.h"
 
 /* The C function */
     int sum_of_three(int arg1, int arg2, int arg3)
     {
+    	
+    	segments_install_gdt();
     	char buffer[] = "Feel the Wind";
     	
   	fb_write(buffer, 19);
   	serial_write(0x3F8, buffer, 19);
+  	
     	
         return arg1 + arg2 + arg3;
     }
